d81d2f6df292d58a4f25b881ada89091aed750d0
https://github.com/alfa546 <noumansajid623@gmail.com>
2026-07-20 22:41:51 +0500

    chore: remove all codebase files except README

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: cd backend && sh run.sh
--- backend/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Start Celery worker in background using solo pool (recommended for Heroku single-dyno setup)
-python -m celery -A celery_app worker --loglevel=info --pool=solo &
-
-# Start FastAPI web server
-uvicorn main:app --host 0.0.0.0 --port $PORT
